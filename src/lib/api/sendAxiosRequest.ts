c34c6e4a9cfafd0325d5cad6bbc6b56092863df6
eprnf <eprnf21@gmail.com>
2024-06-06 00:24:02 +0900

    refactor: axios instance에 토큰 확인 추가

---
+++ src/lib/api/sendAxiosRequest.ts
@@ -0,0 +1,62 @@
+import axios, { AxiosRequestConfig } from "axios";
+import { getCookie } from "cookies-next";
+import { APP_BASE_URL } from "@/constants/common";
+
+export const axiosInstance = axios.create({
+  baseURL: APP_BASE_URL,
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: false,
+});
+
+axiosInstance.interceptors.request.use(function (config) {
+  // 요청이 전달되기 전, 요청(config)에 대한 설정 작업
+  const accessToken = getCookie("accessToken");
+  const refreshToken = getCookie("refreshToken");
+
+  if (refreshToken) config.headers.Refresh = refreshToken;
+  if (accessToken) config.headers.Authorization = `Bearer ${accessToken}`;
+
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      const refreshToken = originalRequest.headers["Refresh"];
+
+      if (!refreshToken) {
+        return Promise.reject(error);
+      }
+
+      try {
+        const response = await axiosInstance.post("/auth/refresh-token", {
+          refreshToken: refreshToken,
+        });
+
+        const newAccessToken = response.data.accessToken;
+
+        // 새로 발급된 accessToken으로 원래 요청을 다시 시도.
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        return axiosInstance(originalRequest);
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  },
+);
+
+const sendAxiosRequest = async (options: AxiosRequestConfig) => {
+  const client = axiosInstance({ ...options });
+  await client;
+  return client;
+};
+
+export default sendAxiosRequest;
