197210df6463087c7451dffa8c346044d6b777d2
eprnf <eprnf21@gmail.com>
2024-06-06 14:58:11 +0900

    fix: refresh토큰 오류 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/Layout/Header";
 import Container from "@/components/Layout/Container";
 import Footer from "@/components/Layout/Footer";
 import "./globals.css";
+import { AuthProvider } from "@/contexts/AuthProvider";
 
 export const metadata: Metadata = {
   title: "판다마켓",
@@ -20,9 +21,11 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body>
-        <Header />
-        <Container>{children}</Container>
-        <Footer />
+        <AuthProvider>
+          <Header />
+          <Container>{children}</Container>
+          <Footer />
+        </AuthProvider>
       </body>
     </html>
   );
--- src/lib/api/sendAxiosRequest.ts
+++ src/lib/api/sendAxiosRequest.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosRequestConfig } from "axios";
-import { getCookie } from "cookies-next";
+import { getCookie, setCookie } from "cookies-next";
 import { APP_BASE_URL } from "@/constants/common";
 
 export const axiosInstance = axios.create({
@@ -14,9 +14,7 @@ export const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(function (config) {
   // 요청이 전달되기 전, 요청(config)에 대한 설정 작업
   const accessToken = getCookie("accessToken");
-  const refreshToken = getCookie("refreshToken");
 
-  if (refreshToken) config.headers.Refresh = refreshToken;
   if (accessToken) config.headers.Authorization = `Bearer ${accessToken}`;
 
   return config;
@@ -29,7 +27,7 @@ axiosInstance.interceptors.response.use(
     if (error.response?.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
 
-      const refreshToken = originalRequest.headers["Refresh"];
+      const refreshToken = getCookie("refreshToken");
 
       if (!refreshToken) {
         return Promise.reject(error);
@@ -41,9 +39,9 @@ axiosInstance.interceptors.response.use(
         });
 
         const newAccessToken = response.data.accessToken;
+        setCookie("accessToken", newAccessToken);
 
         // 새로 발급된 accessToken으로 원래 요청을 다시 시도.
-        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
         return axiosInstance(originalRequest);
       } catch (refreshError) {
         return Promise.reject(refreshError);
