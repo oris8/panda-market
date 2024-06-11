b995fd22488ab2e2f5de4b2942f8dff58f1cb78c
eprnf <eprnf21@gmail.com>
2024-06-11 22:16:37 +0900

    refactor: pagination 수정

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -35,6 +35,41 @@ const Pagination = ({
     ));
   };
 
+  const renderPageButtonEllipsis = () => {
+    return Array.from({ length: totalPages }, (_, i) => {
+      if (
+        i === 0 ||
+        i === totalPages - 1 ||
+        Math.abs(i + 1 - currentPage) < 2
+      ) {
+        return (
+          <button
+            key={i + 1}
+            onClick={goToPage ? () => goToPage(i + 1) : undefined}
+            className={`${buttonStyle} ${
+              i + 1 === currentPage ? "bg-blue text-white" : ""
+            }`}
+          >
+            {i + 1}
+          </button>
+        );
+      } else if (
+        (i === 1 && currentPage > 3) ||
+        (i === totalPages - 2 && currentPage < totalPages - 3)
+      ) {
+        return (
+          <span
+            key={i + 1}
+            className="flexcenter m-2 h-40 w-40 text-16 font-semibold text-cool-gray-500"
+          >
+            ...
+          </span>
+        );
+      }
+      return null;
+    }).filter(Boolean);
+  };
+
   return (
     <div className={`flexcenter w-full ${className}`}>
       <button
@@ -44,7 +79,7 @@ const Pagination = ({
       >
         <ArrowLeftIcon />
       </button>
-      {renderPageButtons()}
+      <> {totalPages < 6 ? renderPageButtons() : renderPageButtonEllipsis()}</>
       <button
         onClick={goToNextPage}
         disabled={totalPages ? currentPage === totalPages : false}
