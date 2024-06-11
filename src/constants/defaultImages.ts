10511c3fc31448a18d2ba83271cc0654fc1d7930
eprnf <eprnf21@gmail.com>
2024-06-11 22:17:10 +0900

    add: 허용되지 않는 이미지 들어올 경우 기본이미지 보여주도록 변경

---
+++ src/constants/defaultImages.ts
@@ -0,0 +1,6 @@
+export const EMPTY_POST_IMAGE = "/images/img_default.png";
+export const EMPTY_INQUIRY_IMAGE = "/images/img_inquiry_empty.svg";
+export const EMPTY_REPLY_IMAGE = "/images/img_reply_default.png";
+export const DEFAULT_PROFILE_IMAGE = "/images/img_default-profile.svg";
+export const DEFAULT_PROFILE_ACTIVE_IMAGE =
+  "/images/img_default-profile-active.svg";
